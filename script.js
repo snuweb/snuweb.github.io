f300933cb80dec4782ada05053cd312a0a4beb68
Saadiq <43720122+snuweb@users.noreply.github.com>
2024-06-17 12:35:28 +0200

    set up github files in main branch

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// For future enhancements
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Document is ready");
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    header.addEventListener('click', function() {
+        header.style.backgroundColor = header.style.backgroundColor === 'rgb(76, 175, 80)' ? '#555' : '#4CAF50';
+    });
+
 });
+
